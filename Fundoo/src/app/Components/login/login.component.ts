8b1ce80a81c030bee05dc1976d5b6a2680f996f2
Arjunsing1997 <acchur1997@gmail.com>
2021-07-01 21:05:07 +0530

    Fundoo Note with Log in and forget password page

--- Fundoo/src/app/Components/login/login.component.ts
+++ Fundoo/src/app/Components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { from } from 'rxjs';
+import { UserServiceService} from '../../Services/user-service.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  hide=true
+  constructor(private formBuilder: FormBuilder, private userService:UserServiceService,private route:Router) { }
+  auth:boolean = true;
 
-  constructor() { }
+  ngOnInit() {
 
-  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required ]]
+    });
+  }
+  public hasError = (controlName: string, errorName: string) => {
+    return this.loginForm.controls[controlName].hasError(errorName);
   }
 
+  loginUser(loginform) {
+    let reqData = {
+      email: loginform.email,
+      password: loginform.password
+
+    }
+    console.log(reqData);
+
+    this.userService.loginUser(reqData).subscribe((res)=>{
+      console.log(" resgister success full ",res);
+      localStorage.setItem('token',res['id']);
+      
+
+      this.route.navigate(['dashboard'])
+      
+    })
+
+
+  }
+
+  toggleStatus(){
+    this.auth=!this.auth;
+  }
 }
