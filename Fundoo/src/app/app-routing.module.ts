b254bf0b1979aa89cdaa2140dc575c5359ca6808
Arjunsing1997 <acchur1997@gmail.com>
2021-07-06 22:29:23 +0530

    [Added] Dash Board to the Application

--- Fundoo/src/app/app-routing.module.ts
+++ Fundoo/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 
 
 const routes: Routes = [
   {path : 'login', component: LoginComponent},
   {path : 'register', component : RegisterComponent},
+  {path : 'dashBoard', component: DashBoardComponent}
 
 ];
 
--- Fundoo/src/app/app.module.ts
+++ Fundoo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -19,7 +19,6 @@ import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashBoardComponent } from './dash-board/dash-board.component';
 
 
